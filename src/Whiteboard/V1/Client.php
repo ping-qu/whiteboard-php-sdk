bcc640bba94ccb0672023f4da08088eb1ce345e7
yue lin <yuelin@yuedeiMac.local>
2017-07-11 21:27:51 +0800

    init

--- src/Whiteboard/V1/Client.php
+++ src/Whiteboard/V1/Client.php
@@ -8,37 +8,56 @@
  */
 class Client
 {
-    public function __construct($accessKeyId, $accessKeySecret)
+    public function __construct($accessKeyId, $accessKeySecret, $endpoint)
     {
         $this->accessKeyId = $accessKeyId;
         $this->accessKeySecret = $accessKeySecret;
+        $this->endpoint = empty($endpoint)?'http://yun.linyue.hznwce.com':$endpoint;
+    }
+    public function setEndpoint($endpoint=null){
+        $this->endpoint = empty($endpoint)?$this->endpoint:$endpoint;
+    }
+    public function setAccessKeySecret($accessKeySecret=null){
+        $this->accessKeySecret = empty($accessKeySecret)?$this->accessKeySecret:$accessKeySecret;
     }
 
-    public function get(){
-
-    }
 
 
-    public function create(){
-        $api = 'http://yun.linyue.hznwce.com/v4_0/whiteboard/create';
-        $client = new \Pingqu\MultimediaTranscoder\V1\Job($this->accessKeyId, $this->accessKeySecret,$api);
+
+    public function create($name){
+        $client = new \Pingqu\OpenApi\Api($this->accessKeyId, $this->accessKeySecret,'/v4_0/whiteboard/create');
         $params = [
-
+            'name'=>$name
         ];
         $client->set_params($params);
         $respone = $client->send_request('GET');
-        $body= json_decode($respone->body);
-        return $body;
+        $body = json_decode($respone->body);
+        if($body->errorId == 'OK'){
+            return $body->data;
+        }else{
+            throw new \DdvPhp\DdvFile\Exception\Sys('添加失败',$body->message);
+        }
 
     }
 
-    public function edit(){
+    public function pause(){
 
     }
 
 
-    public function destory(){
-
+    public function destory($sid){
+        $client = new \Pingqu\OpenApi\Api($this->accessKeyId, $this->accessKeySecret,'/v4_0/whiteboard/delete');
+        $params = [
+            'sid'=>$sid
+        ];
+        $client->set_params($params);
+        $respone = $client->send_request('DELETE');
+        $body = json_decode($respone->body);
+        if($body->errorId == 'OK'){
+            return $body->data;
+        }else{
+            throw new \DdvPhp\DdvFile\Exception\Sys('删除失败',$body->message);
+        }
     }
 
 
